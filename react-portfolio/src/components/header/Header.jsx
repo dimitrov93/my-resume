fabe17ebae9d87437b805ed7794e8d3405645f38
dimitrov93 <ts.dimitrov93@gmail.com>
2023-02-02 11:01:42 +0200

    header component

--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import CTA from './CTA'
 import './header.css'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Tsvetomir</h1>
+        <h5 className="text-light">Front-end Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll down</a>
+
+      </div>
+    </header>
   )
 }
 
