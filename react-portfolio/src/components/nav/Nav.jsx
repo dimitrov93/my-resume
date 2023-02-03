50e8d2649af0e5e97bc85750428183fda5dd1e45
dimitrov93 <ts.dimitrov93@gmail.com>
2023-02-03 12:07:15 +0200

    navigation react-icons + css

--- react-portfolio/src/components/nav/Nav.jsx
+++ react-portfolio/src/components/nav/Nav.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './nav.css'
+import { AiOutlineHome, AiOutlineUser, AiOutlineMessage  } from "react-icons/ai";
+import { FiBookOpen  } from "react-icons/fi";
+import { RiCustomerServiceLine  } from "react-icons/ri";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome /></a>
+      <a href="#about"><AiOutlineUser /></a>
+      <a href="#experience"><FiBookOpen /></a>
+      <a href="#services"><RiCustomerServiceLine /></a>
+      <a href="#contact"><AiOutlineMessage /></a>
+
+    </nav>
   )
 }
 
