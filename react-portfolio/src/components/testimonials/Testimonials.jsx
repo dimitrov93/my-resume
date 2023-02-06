f7fa6a30ced4c17f5ed787783568c6478ad0eefa
dimitrov93 <ts.dimitrov93@gmail.com>
2023-02-06 13:34:31 +0200

    testimnials comp

--- react-portfolio/src/components/testimonials/Testimonials.jsx
+++ react-portfolio/src/components/testimonials/Testimonials.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
-import './testimonials.css'
+import React from "react";
+import "./testimonials.css";
+import AVT1 from "../../assets/avatar1.jpg";
+import AVT2 from "../../assets/avatar2.jpg";
+import AVT3 from "../../assets/avatar3.jpg";
+import AVT4 from "../../assets/avatar4.jpg";
 
+const data = [
+  {
+    avatar: AVT1,
+    name: "Ts Dimitrov",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci amet culpa maxime nesciunt necessitatibus!",
+  },
+
+  {
+    avatar: AVT2,
+    name: "Ts Dimitrov",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci amet culpa maxime nesciunt necessitatibus!",
+  },
+
+  {
+    avatar: AVT3,
+    name: "Ts Dimitrov",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci amet culpa maxime nesciunt necessitatibus!",
+  },
+
+  {
+    avatar: AVT4,
+    name: "Ts Dimitrov",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci amet culpa maxime nesciunt necessitatibus!",
+  },
+];
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
-  )
-}
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
 
-export default Testimonials
+      <div className="container testimonials__container">
+          {
+            data.map(({avatar,name, review}, index) => {
+              return (
+                <article key={index} className="testimonials">
+                <div className="client__avatar">
+                  <img src={avatar} />
+                </div>
+                <h5 className="client__name">{name}</h5>
+                <small className="client__review">
+                  {review}
+                </small>
+              </article>
+              )
+            })
+          }
+      </div>
+
+    </section>
+  );
+};
+
+export default Testimonials;
