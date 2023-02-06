d614b0d4522a74e9d084b7cf4a5192ecd3a7baa5
dimitrov93 <ts.dimitrov93@gmail.com>
2023-02-06 12:48:27 +0200

    services comp

--- react-portfolio/src/components/services/Services.jsx
+++ react-portfolio/src/components/services/Services.jsx
@@ -1,11 +1,134 @@
-import React from 'react'
-import './services.css'
-
+import React from "react";
+import "./services.css";
+import { BsCheckCircle } from "react-icons/bs";
 
 const Services = () => {
-  return (
-    <section id='services'>Services</section>
-  )
-}
+  return <section id="services">
+    <h5>What I offer</h5>
+    <h2>Services</h2>
 
-export default Services
+    <div className="container services__container">
+      <article className="service">
+        <div className="service__head">
+          <h3>Lorem ipsum dolor sit amet.</h3>
+        </div>
+
+        <div className="service__list">
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          {/* END OF FIRST */}
+        </div>
+      </article>
+
+      <article className="service">
+        <div className="service__head">
+          <h3>Lorem ipsum dolor sit amet.</h3>
+        </div>
+
+        <div className="service__list">
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          {/* END OF FIRST */}
+        </div>
+      </article>
+
+      <article className="service">
+        <div className="service__head">
+          <h3>Lorem ipsum dolor sit amet.</h3>
+        </div>
+
+        <div className="service__list">
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          <li>
+            <BsCheckCircle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+
+          {/* END OF FIRST */}
+        </div>
+      </article>
+    </div>
+  </section>;
+};
+
+export default Services;
