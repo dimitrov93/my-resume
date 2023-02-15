d5168d27d6408abdd14d906b30dc8ef4373297fb
dimitrov93 <ts.dimitrov93@gmail.com>
2023-02-15 11:24:26 +0200

    Update Testimonial.js

--- server/src/models/Testimonial.js
+++ server/src/models/Testimonial.js
@@ -4,19 +4,21 @@ const testimonialSchema = new mongoose.Schema(
   {
     name: {
       type: String,
+      required: true,
     },
     review: {
       type: String,
+      required: true,
     },
     avatar: {
-        type: mongoose.Schema.Types.Mixed,
-        required: true    
+      type: String,
+      default:
+        "https://github.com/dimitrov93/my-resume/blob/main/react-portfolio/src/assets/avatar1.jpg?raw=true",
     },
-
   },
   { timestamps: true }
 );
 
-const Testimonial = mongoose.model('Testimonial', testimonialSchema);
+const Testimonial = mongoose.model("Testimonial", testimonialSchema);
 
 module.exports = Testimonial;
