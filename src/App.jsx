1dcaa84042b2a70f2202593bdefa5f1328d35782
dimitrov93 <ts.dimitrov93@gmail.com>
2023-02-17 19:52:47 +0200

    loggin guard

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,54 @@
-import React from 'react'
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
-import About from './components/about/About'
-import Experience from './components/experience/Experience'
-import Services from './components/services/Services'
-import Portfolio from './components/portfolio/Portfolio'
-import Testimonials from './components/testimonials/Testimonials'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
-import Login from './components/Login/login'
-import Logout from './components/Logout/Logout'
-import AddPortfolio from './components/portfolio/Add/AddPortfolio'
-import TestimonialAdd from './components/testimonials/Add/TestimonialAdd'
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import Login from "./components/Login/login";
+import Logout from "./components/Logout/Logout";
+import AddPortfolio from "./components/portfolio/Add/AddPortfolio";
+import TestimonialAdd from "./components/testimonials/Add/TestimonialAdd";
 import { Routes, Route } from "react-router-dom";
-import { AuthProvider } from './context/AuthContext'
+import { AuthProvider } from "./context/AuthContext";
 import EditPortfolio from "./components/portfolio/Edit/EditPortfolio";
-
-
+import LogginGuard from "./common/logginGuard";
 
 const App = () => {
   return (
     <AuthProvider>
       <Routes>
-        <Route basename={'/'} path='/'
+        <Route
+          basename={"/"}
+          path="/"
           element={
             <>
-            <Header />
-            <Nav />
-            <About />
-            <Experience />
-            <Services />
-            <Portfolio />
-            <Testimonials />
-            <Contact />
-            <Footer />
+              <Header />
+              <Nav />
+              <About />
+              <Experience />
+              <Services />
+              <Portfolio />
+              <Testimonials />
+              <Contact />
+              <Footer />
             </>
-          }/>
+          }
+        />
+        <Route path="/login" element={<Login />} />
 
-      <Route path='/login' element={<Login />} />
-      <Route path="/logout" element={<Logout />} />
-      <Route path="/portfolio/add" element={<AddPortfolio />} />
-      <Route path="/portfolio/edit/:id" element={<EditPortfolio />} />
-      <Route path="/testimonials/add" element={<TestimonialAdd />} />
-          
+        <Route element={<LogginGuard />}>
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/portfolio/add" element={<AddPortfolio />} />
+          <Route path="/portfolio/edit/:id" element={<EditPortfolio />} />
+          <Route path="/testimonials/add" element={<TestimonialAdd />} />
+        </Route>
       </Routes>
     </AuthProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
