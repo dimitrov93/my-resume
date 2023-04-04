f0f97ffb8c32927978a3b0daece1cd0518a959e3
dimitrov93 <ts.dimitrov93@gmail.com>
2023-04-04 10:10:19 +0300

    updating fav ico

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
 import './footer.css'
 import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer>
-      <a href="" className="footer__logo">Footer</a>
-
       <ul className="permalinks">
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><Link to="#">Home</Link></li>
+        <li><Link to="#about">About</Link></li>
+        <li><Link to="#experience">Experience</Link></li>
+        <li><Link to="#services">Services</Link></li>
+        <li><Link to="#portfolio">Portfolio</Link></li>
+        <li><Link to="#testimonials">Testimonials</Link></li>
+        <li><Link to="#contact">Contact</Link></li>
       </ul>
 
       <div className="footer__socials">
-        <a href="https://facebook.com"><FaFacebookF /> </a>
-        <a href="https://instagram.com"><FaInstagram /></a>
-        <a href="https://linkedin.com"><FaLinkedinIn /></a>
+        <Link to="https://www.facebook.com/dimitrovtsvetomir" target="_blank"><FaFacebookF /></Link>
+        <Link to="https://www.instagram.com/cvetomir.dimitrov" target="_blank"><FaInstagram /></Link>
+        <Link to="https://www.linkedin.com/in/tsvetomir-dimitrov-921678238" target="_blank"><FaLinkedinIn /></Link>
       </div>
 
       <div className="footer__copyrights">
